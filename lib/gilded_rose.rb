bbb6c9a3c892eaad17b9f29d496d2bb8b4681331
Maria Kennedy <mariakingsbury1@gmail.com>
2024-03-14 22:12:23 -0500

    item class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,43 +1,15 @@
-class GildedRose
-  attr_reader :item
-
-  def initialize(name, quality, days_remaining)
-    @item = klass_for(name).new(quality, days_remaining)
-  end
-
-  def klass_for(name)
-    case name
-    when "Normal Item"
-      Normal
-    when "Aged Brie"
-      Brie
-    when "Sulfuras, Hand of Ragnaros"
-      Sulfuras
-    when "Backstage passes to a TAFKAL80ETC concert"
-      Backstage
-    end
-  end
-
-  def tick
-    item.tick
-  end
-
-  def quality
-    return item.quality
-  end
-
-  def days_remaining
-    return item.days_remaining
-  end
-
-  # add a new class for Normal within GildedRose
-  class Normal
+module GildedRose
+  class Item
     attr_reader :quality, :days_remaining
 
     def initialize(quality, days_remaining)
       @quality, @days_remaining = quality, days_remaining
     end
 
+    def tick
+    end
+  end
+  class Normal < Item
     def tick
       @days_remaining -= 1
       return if @quality == 0
@@ -45,16 +17,9 @@ class GildedRose
       @quality -= 1
       @quality -= 1 if @days_remaining <= 0
     end
-
   end
 
-  class Brie
-    attr_reader :quality, :days_remaining
-
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-
+  class Brie < Item
     def tick
       @days_remaining -= 1
       return if @quality >= 50
@@ -62,27 +27,15 @@ class GildedRose
       @quality += 1
       @quality += 1 if @days_remaining <= 0 && @quality < 50 # per announcement
     end
-
   end
 
-  class Sulfuras
-    attr_reader :quality, :days_remaining
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-
+  class Sulfuras < Item
     def tick
       # do nothing
     end
   end
 
   class Backstage
-    attr_reader :quality, :days_remaining
-
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-
     def tick
       @days_remaining -= 1
       return      if @quality >= 50
@@ -92,7 +45,22 @@ class GildedRose
       @quality += 1 if @days_remaining < 10
       @quality += 1 if @days_remaining < 5
     end
-
   end
 
+def self.new(name, quality, days_remaining)
+  klass_for(name).new(quality, days_remaining)
+end
+
+def self.klass_for(name)
+  case name
+  when "Normal Item"
+    Normal
+  when "Aged Brie"
+    Brie
+  when "Sulfuras, Hand of Ragnaros"
+    Sulfuras
+  when "Backstage passes to a TAFKAL80ETC concert"
+    Backstage
+  end
+end
 end
