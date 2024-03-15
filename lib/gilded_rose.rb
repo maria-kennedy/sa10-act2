47d5a5fcdf155796cc2b0ef1fafcaedb2789acff
Maria Kennedy <mariakingsbury1@gmail.com>
2024-03-14 19:15:38 -0500

    initial commit

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,6 +8,11 @@ class GildedRose
   end
 
   def tick
+    # make 1st 'seam'
+    if name == "normal"
+      return
+    end
+
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
         if @name != "Sulfuras, Hand of Ragnaros"
@@ -52,4 +57,11 @@ class GildedRose
       end
     end
   end
+  
+  # added new method
+  def normal_tick
+    @quality -= 1
+    @days_remaining -= 1
+  end
+
 end
