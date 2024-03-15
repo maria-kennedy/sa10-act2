626c2e30cccb00d498ff9d0d8ca8009ee0e0c08e
Maria Kennedy <mariakingsbury1@gmail.com>
2024-03-14 21:14:36 -0500

    Added new class Normal

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,75 +1,26 @@
 class GildedRose
-  attr_reader :name, :days_remaining, :quality
-
-  def initialize(name:, days_remaining:, quality:)
-    @name = name
-    @days_remaining = days_remaining
-    @quality = quality
+  def normal_tick
+    @item = Normal.new(quality, days_remaining)
+    item.tick
   end
+  
+  # add a new class for Normal within GildedRose
+  class Normal
+    attr_reader :quality, :days_remaining
 
-
-
-  def tick
-    # # make 1st 'seam'
-    # if name == "normal"
-    #   return
-    # end
-
-    # adding case for tracking
-    case name
-    when "Normal Item"
-      return normal_tick
-    when "Aged Brie"
-      return brie_tick
-    when "Sulfuras, Hand of Ragnaros"
-      return sulfuras_tick
-    when "Backstage passes to a TAFKAL80ETC concert"
-      return backstage_tick
+    def initialize(quality:, days_remaining:)
+      @quality = quality
+      @days_remaining = days_remaining
     end
 
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
-      end
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
     end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
-      end
-    end
+
   end
 
   # refactored normal_tick
